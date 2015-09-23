e64f2a4f6098e2f173223ff91e44e39e283afcc2
florent Peyron <florentpeyron@gmail.com>
2015-09-23 06:09:44 +0200

    [ARCH] add callback

--- mock-ttis/src/main/java/fr/boursorama/bus/mock/ttis/ConsumerRoutebuilder.java
+++ mock-ttis/src/main/java/fr/boursorama/bus/mock/ttis/ConsumerRoutebuilder.java
@@ -1,7 +1,11 @@
 package fr.boursorama.bus.mock.ttis;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.ExchangePattern;
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
+
 
 /**
  * @author fpeyron
@@ -13,13 +17,27 @@ public class ConsumerRoutebuilder extends RouteBuilder {
     public void configure() throws Exception {
 
         from("cxf:bean:ttis.SMPCardServices")
-                .to("log:ttis.SMPCardServices.input?level=INFO&showBody=true&showHeaders=true")
+                //.to("log:ttis.SMPCardServices.input?level=INFO&showBody=true")
                 .transform().xpath("//messageSMPAllerXML/*")
                 .convertBodyTo(String.class)
                 .to("validator:xsd/CSD002Aller.xsd")
+                .to(ExchangePattern.InOnly, "seda:response")
                 .to("xslt:xsl/ttisMock.xsl")
-                .to("log:ttis.SMPCardServices.output?level=INFO&showBody=true")
+                //.to("log:ttis.SMPCardServices.output?level=INFO&showBody=true")
         ;
 
+        from("seda:response")
+                .to("xslt:xsl/brsMock.xsl")
+                .removeHeaders("*", "callback.*")
+                .choice()
+                    .when(header("callback.url"))
+                    .setHeader(Exchange.DESTINATION_OVERRIDE_URL).simple("${in.header.callback.url}")
+                .end()
+                .choice()
+                    .when(header("callback.sleep"))
+                    .delay().simple("${in.header.callback.sleep}")
+                .end()
+                .to("cxf:bean:brs.SMPCardServices")
+        ;
     }
 }
