de47fe6536e294f463c6175395ecd678884deb9b
florent Peyron <florentpeyron@gmail.com>
2015-09-24 23:40:30 +0200

    [ARCH] update ttis interface

--- interface-ttis/src/main/java/fr/boursorama/bus/ttis/ConsumerRoutebuilder.java
+++ interface-ttis/src/main/java/fr/boursorama/bus/ttis/ConsumerRoutebuilder.java
@@ -1,6 +1,6 @@
 package fr.boursorama.bus.ttis;
 
-import org.apache.camel.Exchange;
+import fr.boursorama.bus.util.broker.BrokerUtil;
 import org.apache.camel.ExchangePattern;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
@@ -16,14 +16,45 @@ public class ConsumerRoutebuilder extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
+        from(BrokerUtil.consumer("ttis.consumer"))
+                .to("log:brs.call.SMPCardServices.input?level=INFO&showBody=true")
+                //.setHeader("messageType").xpath("local-name(/*[1])", String.class)
+                //.to("validator:xsd/CSD002Aller.xsd")
+                .setBody().constant("<gen:activer xmlns:gen=\"http://generic.monetiq.evolan.sopra.com/\">\n" +
+                "         <messageSMPAllerXML>\n" +
+                "            <CSD002Aller>\n" +
+                "            \t<serviceMetierPublicAller>\n" +
+                "            \t\t<refExtEmetteur>myRef</refExtEmetteur>\n" +
+                "            \t\t<idService>232</idService>\n" +
+                "            \t\t<version>1.0</version>\n" +
+                "            \t\t<userid>012121</userid>\n" +
+                "            \t\t<codLang>23232</codLang>\n" +
+                "            \t\t<refExtDemSMP>2121</refExtDemSMP>\n" +
+                "            \t</serviceMetierPublicAller>\n" +
+                "            \t<numPan>1212323453535</numPan>\n" +
+                "            \t<datFinValidite>201602</datFinValidite>\n" +
+                "            \t<codDemande>A232</codDemande>\n" +
+                "            \t<codCanalAcquisition>21DSD22</codCanalAcquisition>\n" +
+                "            </CSD002Aller>\n" +
+                "         </messageSMPAllerXML>\n" +
+                "      </gen:activer>")
+                .to("cxf:bean:ttis.SMPCardServices")
+                .to("log:brs.call.SMPCardServices.output?level=INFO&showBody=true")
+        ;
+
         from("cxf:bean:brs.SMPCardServices")
+                .to("log:brs.SMPCardServices.input?level=INFO&showBody=true")
                 .transform().xpath("//messageSMPAllerXML/*")
                 .convertBodyTo(String.class)
+                .setHeader("messageType").xpath("local-name(/*[1])", String.class)
+                .setHeader("messageVersion").xpath("local-name(/*[1]/serviceMetierPublicRetour/version/text())", String.class)
+                //.log(LoggingLevel.INFO,"${header.messageType} ${header.messageVersion}")
                 .to("validator:xsd/CSD002Retour.xsd")
-                //.to("xslt:xsl/brsMock.xsl")
-                //.convertBodyTo(String.class)
-                .transform().simple("<gen:notifierResponse xmlns:gen=\"http://generic.ttis.bus.boursorama.fr/\"><messageSMPRetourXML /></gen:notifierResponse>")
-                .to("log:ttis.SMPCardServices.output?level=INFO&showBody=true")
+                        //.to("xslt:xsl/brsMock.xsl")
+                        //.convertBodyTo(String.class)
+                .to(ExchangePattern.InOnly, BrokerUtil.producer("ttis.producer"))
+                .transform().constant("<gen:notifierResponse xmlns:gen=\"http://generic.ttis.bus.boursorama.fr/\"><messageSMPRetourXML /></gen:notifierResponse>")
+                .to("log:brs.SMPCardServices.output?level=INFO&showBody=true")
         ;
 
     }
